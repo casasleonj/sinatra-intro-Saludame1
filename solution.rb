c4b46f3b1a7165d38a638679792461c2158c1270
Jhonnatan Casas <32658189+casasleonj@users.noreply.github.com>
2018-04-03 09:28:48 -0500

    Update solution.rb

--- solution.rb
+++ solution.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 get '/' do
   nombre = params[:nombre]
   if nombre
-  	"<h1>hola #{nombre}!</h1>"
+  	"<h1>hola #{nombre.capitalize}!</h1>"
   else
   	"<h1>hola desconocido</h1>"
   end
