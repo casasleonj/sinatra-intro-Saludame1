9d459387f4f42f26bd2efac73bc18950ab09b434
Jhonnatan Casas <32658189+casasleonj@users.noreply.github.com>
2018-04-03 10:24:54 -0500

    Update solution.rb

--- solution.rb
+++ solution.rb
@@ -1,12 +1,12 @@
 require 'sinatra'
 #ruta
 get '/' do
-unless params[:nombre] || params[:nombre] == "" 
+#unless params[:nombre] || params[:nombre] == "" 
   "<h1>Hola desconocido!</h1>"
-else
-  "<h1>Hola #{params[:nombre].capitalize}!</h1>"
-end
-end
-#get '/:nombre' do
-#	"<h1>Hola #{params[:nombre].capitalize}!</h1>"
+#else
+ # "<h1>Hola #{params[:nombre].capitalize}!</h1>"
 #end
+end
+get '/:nombre' do
+	"<h1>Hola #{params[:nombre].capitalize}!</h1>"
+end
